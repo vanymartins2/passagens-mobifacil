c850c6e4d4092929e1aa1de2e8ad41922080bd3d
Vanessa Martins <vanymartins2@gmail.com>
2023-10-10 12:26:08 -0300

    added: modal styles

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -17,12 +17,27 @@ export function Modal() {
           top: '50%',
           left: '50%',
           transform: 'translateX(-50%) translateY(-50%)',
+          backgroundColor: '#18191A',
+          padding: '1.5rem',
+          borderRadius: '0.5rem',
         }}
       >
         <AlertDialog.Title>Cancelar Viagem</AlertDialog.Title>
         <AlertDialog.Description>
           Você tem certeza que deseja cancelar esta viagem?
         </AlertDialog.Description>
+
+        <div
+          style={{
+            display: 'flex',
+            gap: 12,
+            justifyContent: 'flex-end',
+            marginTop: '32px',
+          }}
+        >
+          <AlertDialog.Cancel>Cancelar</AlertDialog.Cancel>
+          <AlertDialog.Action id="danger">Confirmar</AlertDialog.Action>
+        </div>
       </AlertDialog.Content>
     </AlertDialog.Portal>
   )
